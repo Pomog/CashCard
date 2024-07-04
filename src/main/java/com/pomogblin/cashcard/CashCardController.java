23d2b4e697774ab3fde653fb4730011c22bb68f0
Yurii Panasuik <thoryur@gmail.com>
2024-07-04 08:03:34 +0300

    GET many

--- src/main/java/com/pomogblin/cashcard/CashCardController.java
+++ src/main/java/com/pomogblin/cashcard/CashCardController.java
@@ -1,10 +1,15 @@
 package com.pomogblin.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -32,9 +37,15 @@ class CashCardController {
         return ResponseEntity.created(locationOfNewCashCard).build();
     }
     
-    @GetMapping()
-    private ResponseEntity<Iterable<CashCard>> findAll() {
-        return ResponseEntity.ok(cashCardRepository.findAll());
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 
 }
